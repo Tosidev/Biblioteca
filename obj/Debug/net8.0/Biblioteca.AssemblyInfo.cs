7cd358540a2d420dcf14c7f256b6afab451c3b4d
Tosidev <tosimarlon@hotmail.com>
2024-10-13 14:40:11 -0300

    Dropdown FormaCompra

--- obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b660b016fa514a065de74f8c8f565746a9e79a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be5939834be10640afad7ed10e42106752e4443")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
